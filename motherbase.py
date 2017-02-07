74d6328055b6654a26b5ed628e0274d0867d0b2c
Mister Pibbs <bqeye17@gmail.com>
2017-02-07 14:07:09 -0500

    Update motherbase.py

--- motherbase.py
+++ motherbase.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import time 
-GPIO.setmode(GPIO.BCM) 
+GPIO.setmode(GPIO.BOARD) 
 GPIO.setwarnings(FALSE) 
-GPIO.setup(17,GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)
 fobone = True 
 while fobone:
   on = "on" 
@@ -10,11 +10,11 @@ while fobone:
   c = "c"
   var = input(["On or Off?"])
   if var == on: 
-    GPIO.output(17,LOW)
+    GPIO.output(11,LOW)
     print "LED on"
     continue
   elif var == off:
-    GPIO.output(17,HIGH)
+    GPIO.output(11,HIGH)
     print "LED off" 
     continue 
   elif var == c:
