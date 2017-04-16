372f23c552feb307045762cebc0f4d0bc83f0d7d
Mister Pibbs <bqeye17@gmail.com>
2017-04-15 22:46:47 -0400

    Update mobosend.py

--- mobosend.py
+++ mobosend.py
@@ -1,10 +1,21 @@
 import socket 
 
-UDP_IP = ""
-UDP_PORT = "" 
-CMD = "" 
+UDP_IP = "10.0.0.151"
+UDP_PORT = 5005 
+CMD = "11" 
 
 sock = socket.socket(socket.AF_INET,
                      socket.SOCK_DGRAM)
-                     
-sock.sendto(CMD, (UDP_IP, UDP_PORT))
+
+while True: 
+  on = "on"
+  off = "off"
+  var = input(["On or Off?])
+               if var == on:
+               print "Sending"
+               sock.sendto(CMD, "1", (UDP_IP, UDP_PORT))
+               
+               elif var == off:
+               print "Sending"
+               sock.sendto(CMD, "0" (UDP_IP, UDP_PORT))
+
