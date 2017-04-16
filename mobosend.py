5e6d73df7c7c3ad4a8d3c6af436849da1508aa0a
Mister Pibbs <bqeye17@gmail.com>
2017-04-16 19:16:24 +0000

    On branch local-raspi

--- mobosend.py
+++ mobosend.py
@@ -2,20 +2,20 @@ import socket
 
 UDP_IP = "10.0.0.151"
 UDP_PORT = 5005 
-CMD = "11" 
-
+CMD = "0" 
+CMDOFF = "1"
 sock = socket.socket(socket.AF_INET,
                      socket.SOCK_DGRAM)
 
 while True: 
   on = "on"
   off = "off"
-  var = input(["On or Off?])
-               if var == on:
-               print "Sending"
-               sock.sendto(CMD, "1", (UDP_IP, UDP_PORT))
+  var = input(["On or Off?"])
+   if var == on:
+   print "Sending"
+   sock.sendto(CMD, (UDP_IP, UDP_PORT))
                
-               elif var == off:
-               print "Sending"
-               sock.sendto(CMD, "0" (UDP_IP, UDP_PORT))
+   elif var == off:
+   print "Sending"
+   sock.sendto(CMDOFF, (UDP_IP, UDP_PORT))
 
