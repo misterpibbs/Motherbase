3caee735469a1aa58a966682ed094456b1293b6e
misterpibbs <misterpibbs@github.com>
2017-05-23 19:33:10 +0000

    Physical Switch script switch.py updated

--- switch.py
+++ switch.py
@@ -1,18 +1,20 @@
 import RPi.GPIO as GPIO 
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False) 
+GPIO.setwarnings(False)
 GPIO.setup(13, GPIO.IN)
-GPIO.setup(16, GPIO.IN) 
+GPIO.setup(16, GPIO.IN)
 GPIO.setup(32, GPIO.OUT) 
 
 GPIO.add_event_detect(13, GPIO.RISING)
 GPIO.add_event_detect(16, GPIO.RISING) 
 
 while True: 
-
+ 
  if GPIO.event_detected(13):
-   GPIO.output(32, GPIO.HIGH) 
-   
+  GPIO.output(32, GPIO.HIGH) 
+ 
+
  elif GPIO.event_detected(16): 
-   GPIO.output(32, GPIO.LOW) 
+  GPIO.output(32, GPIO.LOW) 
+ 
