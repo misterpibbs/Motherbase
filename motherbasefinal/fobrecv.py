fa2784d909b5da76027015401ce379cae9b433b5
misterpibbs <misterpibbs@github.com>
2017-07-23 13:57:36 +0000

    Addedd living room support, doesn't work though. Unsure why

--- motherbasefinal/fobrecv.py
+++ motherbasefinal/fobrecv.py
@@ -6,7 +6,9 @@ import time
 # Set-up gpio channel
 GPIO.setmode(GPIO.BOARD) 
 GPIO.setwarnings(False)
-GPIO.setup(10, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(10, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 
 #sleeps the script for one minute allowing for interface to start and pull in a signal
 #time.sleep(30)
@@ -31,6 +33,12 @@ while True:
 
  elif data == 1: 
   GPIO.output(10, 1)
+
+ elif data == 2:
+  GPIO.output(11, 0)
+
+ elif data == 3:
+  GPIO.output(11, 1)
 
 else:
  conn.close()
