6f8bcf83d274fe4868a6ca188a2b621dae7c3b9c
Mister Pibbs <bqeye17@gmail.com>
2017-07-24 23:29:30 +0000

    GUI, at it's most basic level, is complete for the current home layout. Used .grid to get a basic organization down. The project in theory is complete. But more will be built on. Adding a secondary pi for the other side of the house soon. Woot :)

--- motherbasefinal/motherbase.py
+++ motherbasefinal/motherbase.py
@@ -15,7 +15,6 @@ GPIO.setup(10, GPIO.OUT)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
-
 class Application(Frame):
 
     def bOn(self):   
@@ -41,35 +40,35 @@ class Application(Frame):
         self.QUIT["bg"] = "gray"
         self.QUIT["command"] = self.quit
    
-        self.QUIT.pack(fill=X,padx=10)
+        self.QUIT.grid(row=2, columnspan=2)
   
         self.bO = Button(self)
         self.bO["text"] = "Basement On"
         self.bO["fg"] = "green"
         self.bO["command"] = self.bOn
 
-        self.bO.pack(fill=X,padx=10)
+        self.bO.grid(row=0, column=0)
 
         self.bF = Button(self)
         self.bF["text"] = "Basement Off"
         self.bF["fg"] = "red"
         self.bF["command"] = self.bOff
 
-        self.bF.pack(fill=X,padx=10)
+        self.bF.grid(row=1, column=0)
         
         self.lO = Button(self) 
         self.lO["text"] = "Living Room On"
         self.lO["fg"] = "green" 
         self.lO ["command"] = self.lOn
 
-        self.lO.pack(fill=X,padx=10)
+        self.lO.grid(row=0, column=1)
 
         self.lf = Button(self)
         self.lf["text"] = "Living Room Off"
         self.lf["fg"] = "red"
         self.lf["command"] = self.lOff
 
-        self.lf.pack(fill=X,padx=10)
+        self.lf.grid(row=1, column=1)
          
     def __init__(self, master=None):
         Frame.__init__(self, master)
