26a313cde952d1ffe5cb1aecfba76847df2e8322
misterpibbs <misterpibbs@github.com>
2017-07-25 03:53:25 +0000

    Took out useless comments.

--- motherbasefinal/fobrecv.py
+++ motherbasefinal/fobrecv.py
@@ -2,18 +2,13 @@ import socket
 import RPi.GPIO as GPIO 
 import time  
 
-# Server Side
-# Set-up gpio channel
 GPIO.setmode(GPIO.BOARD) 
 GPIO.setwarnings(False)
 GPIO.setup(10, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(12, GPIO.OUT)
 
-#sleeps the script for one minute allowing for interface to start and pull in a signal
-#time.sleep(30)
 
-# Socket Setup
 TCP_IP = "10.0.0.11"
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
